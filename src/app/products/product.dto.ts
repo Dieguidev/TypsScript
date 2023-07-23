4a38ffa7589953d754ade432237a044b5d9a8e40
Dieguidev <diegogaraycullas@gmail.com>
2023-07-23 08:34:28 -0500

    readonlyArray

--- src/app/products/product.dto.ts
+++ src/app/products/product.dto.ts
@@ -8,4 +8,7 @@ type example = Pick<Product, 'color' | 'description'>;
 
 export interface UpdateProductDto extends Partial<CreateProductDto>{}
 
-export interface FindProductDto extends Readonly<Partial<Product>>{}
+export interface FindProductDto extends Readonly<Partial<Omit<Product, 'tags'>>>{
+  readonly tags: ReadonlyArray<string>;
+
+}
