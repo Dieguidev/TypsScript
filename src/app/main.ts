08e96b5258d2af2f36009d23a7c2e5f63d97cbd0
Dieguidev <diegogaraycullas@gmail.com>
2023-07-22 21:41:00 -0500

    omit

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,23 @@
-import { addProduct } from "./products/product.service";
+import { faker } from '@faker-js/faker/locale/es';
 
-addProduct({
-  id: '1',
-  title: 'p1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 90,
-  category: {
-    id: '12',
-    name: 'c1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }
-})
+import { addProduct, products } from './products/product.service';
+
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    description: faker.commerce.productDescription(),
+    title: faker.commerce.productName(),
+    image: faker.image.url(),
+    stock: faker.number.int({ min: 10, max: 100 }),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    color: faker.color.human(),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(['new', 'white', 'tshirt', 'clothes']),
+    categoryId: faker.datatype.uuid.toString()
+  });
+
+
+}
+
+console.log(products);
+
